26f2b57aaab8b2523784e3e83713adb18d20788d
javad <javadgt02@gmail.com>
2022-05-24 05:12:12 -0700

    third commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import a , {Test}  from './modul1.js'
+
+console.log(new Test(1)); 
+
+console.log(a   );
---
+++ js/modul1.js
@@ -0,0 +1,13 @@
+export  class Test{
+    constructor(num){
+        this.num = num
+    }
+
+    show(){
+        console.log('show something');
+    }
+}
+
+let f = 200
+
+export default f
